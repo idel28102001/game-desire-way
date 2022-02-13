dc5c9dfd31d09919a92102317540c0fa3c14c274
Идель Фархутдинов <idel28102001@mail.ru>
2022-02-14 00:59:14 +0300

    nice

--- src/scripts/card.js
+++ src/scripts/card.js
@@ -1,25 +1,44 @@
 import { el, setChildren } from "redom";
 import { links } from "./db";
+import { resetNew, getArrayLocal, getNumber } from "./randoms";
 import "babel-polyfill";
-export function createCard(name, dict) {
+export function createCard(name, dict, items) {
   const token = `5284035313:AAE15tnvTYt5FAquffIzhWYVAWyTEb3WB_Y`;
   return el("div.card", [
     el("h3.card__heading", { textContent: name }),
     el("div.card__buttons buttons", [
-      createBtn(token, dict.id, "Волшебное перо", "magicLeafs"),
-      createBtn(token, dict.id, "Маккарта", `macCards`),
-      createBtn(token, dict.id, "Эмоция", "emotions"),
-      createBtn(token, dict.id, "Цвет", "colors"),
+      createBtn(token, dict.id, "Волшебное перо", "magicLeafs", {
+        local: "leafs",
+        elem: items.leafs,
+      }),
+      createBtn(token, dict.id, "Маккарта", `macCards`, {
+        local: "maccards",
+        elem: items.card,
+      }),
+      createBtn(token, dict.id, "Эмоция", "emotions", {
+        local: "emotions",
+        elem: items.emotions,
+      }),
+      createBtn(token, dict.id, "Цвет", "colors", { type: "colors" }),
       createBtn(token, dict.id, "Счастливчик", "happyOne"),
     ]),
   ]);
 }
 
-function createBtn(token, id, text, type = "") {
+function createBtn(token, id, text, type = "", dict = {}) {
   const elem = el("button.buttons__item", { textContent: text });
   elem.addEventListener("click", () => {
     if (type) {
-      sendPhoto(token, id, links[type][0]);
+      let number = 0;
+      if (dict.local) {
+        number = getNumber(dict.local);
+        dict.elem.lastChild.textContent =
+          Number(dict.elem.lastChild.textContent) - 1;
+      }
+      if (dict.type) {
+        number = Math.floor(Math.random() * 7);
+      }
+      sendPhoto(token, id, links[type][number]);
     } else {
       sendMessage(token, id, text);
     }
@@ -35,20 +54,124 @@ function getAllUsers(token, callBack, dict) {
     });
 }
 
+function createAdd(text, elem) {
+  const element = el("button.btns-changes__item add", {
+    textContent: text,
+  });
+  element.addEventListener("click", () => {
+    if (element.textContent === text) {
+      element.textContent = "Отмена";
+      elem.classList.add("card__add-active");
+      element.classList.add("add-active");
+    } else {
+      elem.classList.remove("card__add-active");
+      element.classList.remove("add-active");
+      element.textContent = text;
+    }
+  });
+  return element;
+}
+
+function reset(allItems) {
+  const element = el("button.btns-changes__item delete", {
+    textContent: "Сбросить",
+  });
+  element.addEventListener("click", () => {
+    resetNew("leafs", 36, allItems.leafs.lastChild);
+    resetNew("maccards", 50, allItems.card.lastChild);
+    resetNew("emotions", 50, allItems.emotions.lastChild);
+  });
+  return element;
+}
+
+function currItems() {
+  const elements = el("div.all-items");
+  const leafs = el("span.all-items__item all-leaf", [
+    el("h4", { textContent: "Перьев: " }),
+    el("span", { textContent: getArrayLocal("leafs").length }),
+  ]);
+  const card = el("span.all-items__item all-card", [
+    el("h4", { textContent: "Маккарт: " }),
+    el("span", { textContent: getArrayLocal("maccards").length }),
+  ]);
+  const emotions = el("span.all-items__item all-emotions", [
+    el("h4", { textContent: "Эмоций: " }),
+    el("span", { textContent: getArrayLocal("emotions").length }),
+  ]);
+  elements.append(leafs, card, emotions);
+  return { elements, leafs, card, emotions };
+}
+
 export function makeApp() {
   const token = `5284035313:AAE15tnvTYt5FAquffIzhWYVAWyTEb3WB_Y`;
-  const container = el("div");
-  getAllUsers(token, createCurrent, { container: container });
+  const container = el("div.container");
+  const cards = el("div.cards");
+  const allItems = currItems();
+  const create = createAddBtn(token, cards, allItems);
+  const modal = createModale();
+  document.body.append(modal);
+
+  const btns = el("div.btns-changes", [
+    createAdd("Изменить", create),
+    reset(allItems),
+  ]);
+  cards.append(create);
+  container.append(btns, allItems.elements, cards);
   return container;
 }
 
-function createCurrent(listElems, dictElems) {
-  const allNames = getAllNames(listElems);
-  for (let [name, dict] of Object.entries(allNames)) {
-    dictElems.container.append(createCard(name, dict));
-  }
+function createAddBtn(token, cards, items) {
+  const elem = el("div.card card__add");
+  elem.addEventListener("click", () => {
+    const modal = document.querySelector(`.black-back`);
+    modal.classList.add("black-back-active");
+    getAllUsers(token, updateList, {
+      block: modal.firstChild,
+      cards: cards,
+      items: items,
+    });
+  });
+  return elem;
 }
 
+function parseNames(elems) {
+  const newDict = {};
+  Array.from(elems.children)
+    .slice(0, -1)
+    .forEach((e) => {
+      newDict[e.firstChild.textContent] = e;
+    });
+  return newDict;
+}
+
+function updateList(listElems, dict) {
+  dict.block.innerHTML = "";
+  const allNames = getAllNames(listElems);
+  const ul = el("ul.users");
+  const names = parseNames(dict.cards);
+  for (const [key, value] of Object.entries(allNames)) {
+    const li = el("button.users__item", { textContent: key });
+    let blockUser;
+    if (Object.keys(names).includes(key)) {
+      blockUser = names[key];
+      li.classList.add("users__item-active");
+    }
+    li.addEventListener("click", () => {
+      if (li.classList.contains("users__item-active")) {
+        if (blockUser) {
+          blockUser.remove();
+        }
+        li.classList.remove("users__item-active");
+      } else {
+        blockUser = createCard(key, value, dict.items);
+        dict.cards.insertBefore(blockUser, dict.cards.lastChild);
+        li.classList.add("users__item-active");
+      }
+    });
+    ul.append(li);
+  }
+  dict.block.append(ul);
+}
 function sendMessage(token, id, text) {
   fetch(
     `https://api.telegram.org/bot${token}/sendMessage?chat_id=${id}&text=${text}`
@@ -71,4 +194,18 @@ function getAllNames(dict) {
     };
   }
   return newDict;
+}
+
+function createModale() {
+  const block = el("div.block", { id: "block-modal" });
+  const elem = el("div.black-back", [block]);
+  elem.addEventListener("click", (event) => {
+    if (event._isClickWithinModal) return;
+    elem.classList.remove("black-back-active");
+  });
+
+  block.addEventListener("click", (event) => {
+    event._isClickWithinModal = true;
+  });
+  return elem;
 }
---
+++ src/scripts/randoms.js
@@ -0,0 +1,52 @@
+function getLocal(local, counts) {
+  let newArray;
+  if (localStorage.getItem(local)) {
+    newArray = JSON.parse(localStorage.getItem(local));
+  } else {
+    newArray = Array.from(Array(counts + 1).keys());
+    newArray = newArray.slice(1);
+    localStorage.setItem(local, JSON.stringify(newArray));
+  }
+
+  return newArray;
+}
+
+function popNew(elem, array) {
+  return array.filter((e) => {
+    if (e !== elem) {
+      return e;
+    }
+  });
+}
+
+export function resetNew(local, count, elem) {
+  if (elem) {
+    elem.textContent = count;
+  }
+  let newArray = Array.from(Array(count).keys());
+  localStorage.setItem(local, JSON.stringify(newArray));
+}
+
+function update(array, name) {
+  localStorage.setItem(name, JSON.stringify(array));
+  return JSON.parse(localStorage.getItem(name));
+}
+
+export function getNumber(text) {
+  const array = getArrayLocal(text);
+  console.log(text, array);
+  const number = Math.floor(Math.random() * array.length);
+  const getNumber = array[number];
+  update(popNew(getNumber, array), text);
+  return getNumber;
+}
+
+export function getArrayLocal(text) {
+  let local = localStorage.getItem(text);
+  if (local) {
+    local = JSON.parse(local);
+  } else {
+    local = [];
+  }
+  return local;
+}
