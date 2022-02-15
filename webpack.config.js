dcd68d84629fe05113f7493d2cd659f360d013d8
Идель Фархутдинов <idel28102001@mail.ru>
2022-02-16 02:40:13 +0300

    nn

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,37 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = {
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.m?js$/,
-        exclude: /(node_modules|bower_components)/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-          },
-        },
-      },
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
-  plugins: [new HtmlWebpackPlugin()],
-  devServer: {
-    historyApiFallback: true,
-    hot: true,
-  },
-  resolve: {
-    fallback: {
-      crypto: false,
-      fs: false,
-      https: false,
-      stream: false,
-      url: false,
-      path: false,
-      util: false,
-      http: false,
+module.exports = (env) => ({
+    entry: "./src/index.js",
+    output: {
+        filename: "main.js",
+        publicPath: "/",
     },
-  },
-};
+    module: {
+        rules: [
+            // {
+            //       test: /\.m?js$/,
+            //       exclude: /(node_modules|bower_components)/,
+            //       use: {
+            //           loader: "babel-loader",
+            //           options: {
+            //               presets: ["@babel/preset-env"],
+            //           },
+            //       },
+            //   },
+            {
+                test: /\.s?css$/i,
+                use: [
+                    env.prod ? MiniCssExtractPlugin.loader : "style-loader",
+                    "css-loader",
+                    "sass-loader",
+                ],
+            },
+        ],
+    },
+    plugins: [new HtmlWebpackPlugin(), new MiniCssExtractPlugin()],
+    devServer: {
+        historyApiFallback: true,
+        hot: true,
+    },
+});
